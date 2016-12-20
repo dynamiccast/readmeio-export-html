4481366d455b8857f245a6dfb73d74743dabb4f9
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-12-20 14:58:43 +0100

    Use url fragment to determine current page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function selectDoc(name) {
+$(window).on('hashchange', function(e){
+  var hash = location.hash.replace( /^#/, '');
+
+  var target = hash.split('target=')[1];
   $('.doc-page').hide();
-  $('#' + name).show();
-}
+  $('#' + target).show();
+});
