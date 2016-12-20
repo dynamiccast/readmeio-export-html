57d7af77f01f134a3d927a3f696d93babbb4219c
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-12-20 15:00:16 +0100

    Allow to exchange links to the documentation
    Redirect to correct page on first visit if target exists

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(window).on('hashchange', function(e){
+var selectPage = function() {
   var hash = location.hash.replace( /^#/, '');
 
   var target = hash.split('target=')[1];
   $('.doc-page').hide();
   $('#' + target).show();
-});
+};
+
+$(window).on('hashchange', selectPage);
+
+selectPage();
